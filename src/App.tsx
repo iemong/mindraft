e3033e59fb487fe633c134ee239ba1140f21e6f1
iemong <kiyoshidainagon@gmail.com>
2025-04-19 17:36:18 +0900

    feats: add Remirror dependencies and integrate editor in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { BlockquoteExtension, BoldExtension, HeadingExtension, ItalicExtension } from 'remirror/extensions';
+import { Heading, useRemirror } from '@remirror/react';
+import { Remirror } from '@remirror/react';
+import 'remirror/styles/all.css';
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,19 +17,31 @@ function App() {
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  const extensions = () => [new BoldExtension({}), new ItalicExtension(), new BlockquoteExtension(), new HeadingExtension({levels: [1, 2, 3]})]
+
+  const { manager, state } = useRemirror({
+    extensions,
+    content: '<p>I love <b>Remirror</b></p>',
+    selection: 'start',
+    stringHandler: 'html',
+  });
+
   return (
-    <main className="container bg-red-500">
+    <main className="container">
       <h1>Welcome to Tauri + React</h1>
 
       <div className="row">
+        <article className='remirror-theme'>
+        <Remirror manager={manager} initialContent={state} />
+        </article>
         <Button variant={'default'}>Click me</Button>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-        <a href="https://tauri.app" target="_blank">
+        <a href="https://tauri.app" target="_blank" rel="noreferrer">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
